c22f8ec4eb10276b0e8bc14b58d68aba97374b76
Joel0612 <https://github.com/Joel0612/Venta.git>
2025-12-09 19:37:50 -0400

    Actualizar CORS para frontend en Netlify

--- src/main/java/com/inventario/inventario/Config/SecurityConfig.java
+++ src/main/java/com/inventario/inventario/Config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://inventario-7yzy.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/inventario/inventario/controller/ProductoController.java
+++ src/main/java/com/inventario/inventario/controller/ProductoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/productos")
-@CrossOrigin( origins = "http://localhost:5174")
+@CrossOrigin( origins = "https://inventario-7yzy.onrender.com")
 public class ProductoController {
 
     @Autowired
